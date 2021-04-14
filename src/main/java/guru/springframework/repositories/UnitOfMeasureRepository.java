16f3d349946ee94c76802a5ca86d7d7bc0a396f6
XimgXingyue <36713167+XimgXingyue@users.noreply.github.com>
2021-04-14 16:19:30 -0500

    added data initialization script and updated IndexController

--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,6 +1,10 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasureRepository, Long>{
+  Optional<UnitOfMeasure> findByDescription(String description);
 }
