08668ab1732fca6717ab7ecb9a84b5ef5458e8d7
XimgXingyue <36713167+XimgXingyue@users.noreply.github.com>
2021-04-14 18:33:25 -0500

    added unit tests

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.domain;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CategoryTest extends TestCase {
+  Category category;
+
+  @Before
+  public void setUp(){
+    category = new Category();
+  }
+
+  @Test
+  public void getId() throws Exception {
+    Long idValue = 4L;
+
+    category.setId(idValue);
+
+    assertEquals(idValue, category.getId());
+  }
+
+  @Test
+  public void getDescription() throws Exception {
+  }
+
+  @Test
+  public void getRecipes() throws Exception {
+  }
+}
