d707fff3885f817a4ba695622b18fb99787b6340
XimgXingyue <36713167+XimgXingyue@users.noreply.github.com>
2021-04-14 16:41:15 -0500

    refactored IndexController

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -30,8 +31,14 @@ public class Recipe {
   @OneToOne(cascade = CascadeType.ALL)
   private Notes notes;
 
+  @ManyToMany
+  @JoinTable(name = "recipe_category",
+      joinColumns = @JoinColumn(name = "recipe_id"),
+      inverseJoinColumns = @JoinColumn(name = "category_id"))
+  private Set<Category> categories = new HashSet<>();
+
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-  private Set<Ingredient> ingredients;
+  private Set<Ingredient> ingredients = new HashSet<>();
 
   public Long getId() {
     return id;
@@ -119,6 +126,14 @@ public class Recipe {
 
   public void setNotes(Notes notes) {
     this.notes = notes;
+  }
+
+  public Set<Category> getCategories() {
+    return categories;
+  }
+
+  public void setCategories(Set<Category> categories) {
+    this.categories = categories;
   }
 
   public Set<Ingredient> getIngredients() {
